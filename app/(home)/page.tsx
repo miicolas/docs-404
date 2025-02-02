d1e9cd653d9b7a54cff46733af3018336e2e516f
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-02-02 16:46:08 +0100

    init project

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">Docs 404</h1>
       <p className="text-fd-muted-foreground">
-        You can open{' '}
+        Ouvres{' '}
         <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
         >
           /docs
         </Link>{' '}
-        and see the documentation.
+        pour voir la documentation.
       </p>
     </main>
   );
