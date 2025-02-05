3a30a1a023575d8f6308b72c28571e39cfa736c8
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-02-05 20:26:53 +0100

    ci: add dependancies

--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,17 @@
 import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import { remarkAdmonition } from 'fumadocs-core/mdx-plugins';
+import { remarkTypeScriptToJavaScript } from 'fumadocs-docgen';
+
+
+import { remarkInstall } from 'fumadocs-docgen';
 
 export const { docs, meta } = defineDocs({
   dir: 'content/docs',
 });
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkInstall, remarkAdmonition, remarkTypeScriptToJavaScript],
+
+  },
+});
