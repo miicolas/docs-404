e2ddbab766fb31ec0dbd76630db6ec218f79ab10
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-02-05 20:53:02 +0100

    fix: button error

--- app/components/ui/button-github.tsx
+++ app/components/ui/button-github.tsx
@@ -4,11 +4,8 @@ import { SquarePen } from 'lucide-react';
 export default function ButtonGithub({ href }: { href: string }) {
   return (
     <Link href={`https://github.com/miicolas/docs-404/blob/main/content/docs/${href}`} className='flex items-center gap-2 rounded-lg border bg-fd-card p-4 text-sm transition-colors hover:bg-fd-accent/80 hover:text-fd-accent-foreground col-start-2 text-end w-fit font-medium'>
-
       <SquarePen className='w-6 h-6' />
       Edit on GitHub
     </Link>
   );
 }
-
-"/Users/nicolasbecharat/Documents/GitHub/404/docs-404/content/docs/index.mdx"
